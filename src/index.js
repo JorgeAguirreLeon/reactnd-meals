8b61170d98cec0959df134448f8675422f18cedd
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2017-08-05 16:14:04 +0200

    Brought redux and created store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React                      from 'react'
+import ReactDOM                   from 'react-dom'
+import App                        from './components/App'
+import { createStore }            from 'redux'
+import reducer                    from './reducers'
+import registerServiceWorker      from './registerServiceWorker'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
